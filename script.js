05f2b7a00586f08305e65f4ed596f83da5ae92d3
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2021-07-30 15:12:55 +0000

    connected to firebase

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const nameOfCoord = document.querySelector('.name')
+const locationOfCoord = document.querySelector('.loc')
+const heightOfCoord = document.querySelector('.height')
+const northOfCoord = document.querySelector('.north')
+const eastOfCoord = document.querySelector('.east')
+const latOfCoord = document.querySelector('.lat')
+const lonOfCoord = document.querySelector('.lon')
+
+
 // Preparing the map   [51.505,-0.09]
 var mymap = L.map('mapid').setView([6.6745,-1.5716],15)
 
@@ -27,16 +36,13 @@ function onMapClick(e){
     .setLatLng(e.latlng)
     .setContent(`You clicked the map at ${e.latlng}`)
     .openOn(mymap);
-    // alert(`You clicked the map at ${e.latlng}`);
 }
 mymap.on('click',onMapClick);
 
 
- // load GeoJSON from an external file
-$.getJSON("latlng.geojson",function(data){
-    // add GeoJSON layer to the map once the file is loaded
 
-    var geojsonMarkerOptions = {
+
+var geojsonMarkerOptions = {
         radius: 5,
         fillColor: "#ff7800",
         color: "#000",
@@ -44,11 +50,76 @@ $.getJSON("latlng.geojson",function(data){
         opacity: 1,
         fillOpacity: 0.8
     };
-    L.geoJson(data  ,{
-        
-        pointToLayer: function(feature,latlng){
 
-            return L.circleMarker(latlng,geojsonMarkerOptions);
-        }
-    }  ).addTo(mymap);
-});
+db.on('value', (snapshot) => {
+    const data = snapshot.val();
+    data.forEach(element => {
+        // console.log(element)
+        var coords = element.geometry.coordinates
+        // L.marker(coords.reverse()).addTo(mymap);
+        var coordsMarker = L.circleMarker(coords.reverse(),geojsonMarkerOptions).addTo(mymap)
+        // coordsMarker.bindPopup(``)
+        
+        // console.log(coords.reverse())
+
+
+        coordsMarker.on('click', function(e) {
+            nameOfCoord.textContent = element.properties.Name
+            locationOfCoord.textContent =element.properties.Descprition
+            heightOfCoord.textContent = element.properties.height
+            northOfCoord.textContent = element.properties.Y
+            eastOfCoord.textContent = element.properties.X
+            latOfCoord.textContent = element.properties.latitude
+            lonOfCoord.textContent = element.properties.longitude
+
+
+
+
+
+            // console.log('clicked me')
+            // console.log(e)
+            // console.log(coords.reverse())
+        } );
+        
+    });
+   
+    // updateStarCount(postElement, data);
+
+
+  });
+
+  
+
+
+
+
+
+
+
+
+
+ // load GeoJSON from an external file
+// * $.getJSON("latlng.geojson",function(data){
+    // add GeoJSON layer to the map once the file is loaded
+
+// *    var geojsonMarkerOptions = {
+//         radius: 5,
+//         fillColor: "#ff7800",
+//         color: "#000",
+//         weight: 1,
+//         opacity: 1,
+//         fillOpacity: 0.8
+//     };
+//     L.geoJson(data  ,{
+        
+//         pointToLayer: function(feature,latlng){
+
+//             return L.circleMarker(latlng,geojsonMarkerOptions);
+//         }
+//     }  ).addTo(mymap);
+// });
+
+
+
+
+
